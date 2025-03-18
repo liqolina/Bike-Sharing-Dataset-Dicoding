9c517757312be4eaa2f714bf2f006ad6807ca736
Lunar Sandwich <lutfiaunher@gmail.com>
2025-03-18 10:54:22 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -204,7 +204,7 @@ st.pyplot(fig)
 st.subheader("Perbandingan Jumlah Registered dengan Casual")
 
 # Membuat subplot
-fig, ax = plt.subplots()
+fig, ax = plt.subplotsfigsize=(10, 10)()
 
 # Data Untuk Casual
 total_casual = sum(main_df['casual'])
